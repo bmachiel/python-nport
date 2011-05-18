c8d6cc395169229eb1c07b0a3f01b96672db7f33
Brecht Machiels <brecht.machiels@esat.kuleuven.be>
2011-05-18 09:37:06 +0200

    addition of copyright notice, license text and module description

--- nport/__init__.py
+++ nport/__init__.py
@@ -1,3 +1,28 @@
+# Copyright (c) 2010- 2011 Brecht Machiels <brecht.machiels@esat.kuleuven.be>
+#                          ESAT-MICAS, K.U.Leuven
+#
+# This file is part of python-nport (http://github.com/bmachiel/python-nport).
+#
+# python-nport is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-nport is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-nport.  If not, see <http://www.gnu.org/licenses/>.
+
+"""python-nport
+
+nport is a Python package for handling n-port data. It provides abstraction
+for handling frequency dependent n-port parameters. It loads and saves
+Touchstone and CITI files. Additionally, nport provides a set of functions
+relating to RF theory, transmission lines and deembedding.
+"""
 
 try:
     from version import __version__
